72b40dcd498a3b05af0dd8aabc87b1b494df03db
chaitanya_1911 <chaitanya.kumbhar@think360.ai>
2023-04-06 22:25:01 +0530

    fixed bootstrap error

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 import App from "./App";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-    <App />
+      <App />
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
